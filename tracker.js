866410911757d975e4768d863a7fd82505355789
seff0 <76668281+seff0@users.noreply.github.com>
2021-02-24 20:36:17 -0700

    build first inquirer prompt and switch case

--- tracker.js
+++ tracker.js
@@ -19,9 +19,64 @@ function init() {
   //print table to console
 }
 
-// what would you like to do?
-//inquirer prompt for actions
-//switch case for callback functions
+function action() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add a new department",
+        "Add a new role",
+        "Add a new employee",
+        "View employees by department",
+        "View employees by role",
+        "View all employees",
+        "View employees by manager",
+        "View the utilized budget for a department",
+        "Update an employee's role",
+        "Update an employee's manager",
+        "Delete a department",
+        "Delete a role",
+        "Delete an employee",
+        "Exit application",
+      ],
+    })
+    .then((answer) => {
+      let choice = answer.action;
+      switch (choice) {
+        case "Add a new department": {
+        }
+        case "Add a new role": {
+        }
+        case "Add a new employee": {
+        }
+        case "View employees by department": {
+        }
+        case "View employees by role": {
+        }
+        case "View all employees": {
+        }
+        case "View employees by manager": {
+        }
+        case "View the utilized budget for a department": {
+        }
+        case "Update an employee's role": {
+        }
+        case "Update an employee's manager": {
+        }
+        case "Delete a department": {
+        }
+        case "Delete a role": {
+        }
+        case "Delete an employee": {
+        }
+        default: {
+          connection.end();
+        }
+      }
+    });
+}
 
 // add department
 
