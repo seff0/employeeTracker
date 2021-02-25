f4982bc9c0daaf66e9098bcaea49c02bb46d76a1
seff0 <76668281+seff0@users.noreply.github.com>
2021-02-24 20:27:21 -0700

    pseudo code functions

--- tracker.js
+++ tracker.js
@@ -12,5 +12,39 @@ const connection = mysql.createConnection({
 
 connection.connect((err) => {
   if (err) throw err;
-  // initialize
+  // init()
 });
+
+function init() {
+  //print table to console
+}
+
+// what would you like to do?
+//inquirer prompt for actions
+//switch case for callback functions
+
+// add department
+
+// add role
+
+// add employee
+
+// view employees by department
+
+// view employees by role
+
+// view all employees
+
+// view employees by manager
+
+// view utilized department budget (total of all salaries in that department)
+
+// update employee role
+
+// update employee manager
+
+// delete department
+
+// delete role
+
+// delete employee
